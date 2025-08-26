0ab94738bf0e116cf5bf99c2133e1a073e38a278
Andrea Jiménez <andreacjimenez@gmail.com>
2025-08-26 19:51:38 -0400

    dependencias pom.xml

---
+++ rest/src/main/java/cl/kibernum/client/PostClient.java
@@ -0,0 +1,13 @@
+package cl.kibernum.client;
+
+public class PostClient {
+    // Método para listar los posts
+
+    // Método para obtener el detalle de un post por id
+
+    // Método para crear un post
+
+    // Método para actualizar un post
+
+    // Método para eliminar un post
+}
