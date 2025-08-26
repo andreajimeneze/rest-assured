ce6596958920269b1b2e8671a6c5d50536da8a98
Andrea Jiménez <andreacjimenez@gmail.com>
2025-08-26 19:57:14 -0400

    modelo terminado según el json

--- rest/src/main/java/cl/kibernum/client/PostClient.java
+++ rest/src/main/java/cl/kibernum/client/PostClient.java
@@ -2,7 +2,7 @@ package cl.kibernum.client;
 
 public class PostClient {
     // Método para listar los posts
-
+    
     // Método para obtener el detalle de un post por id
 
     // Método para crear un post
